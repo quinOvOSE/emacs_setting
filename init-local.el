b697f23a2eb64902f080e86f693fdc2891d457d8
QuinOvOSE <93961030+quinOvOSE@users.noreply.github.com>
2021-12-21 14:45:39 +0800

    Add files via upload

---
+++ init-local.el
@@ -0,0 +1,87 @@
+;;my own system
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
+(require 'eaf)
+(require 'eaf-browser)
+(require 'eaf-pdf-viewer)
+
+
+(setq eaf-proxy-type "http")
+(setq eaf-proxy-host "127.0.0.1")
+(setq eaf-proxy-port "42705")
+
+(setq org-agenda-span 3)
+(global-set-key (kbd "<M-up>") 'shrink-window)
+(global-set-key (kbd "<M-down>") 'enlarge-window)
+(global-set-key (kbd "<M-left>") 'shrink-window-horizontally)
+(global-set-key (kbd "<M-right>") 'enlarge-window-horizontally)
+
+(require 'posframe)
+(require 'pyim)
+
+(setq default-input-method "pyim")
+
+;; 金手指设置，可以将光标处的编码，比如：拼音字符串，转换为中文。
+(global-set-key (kbd "M-j") 'pyim-convert-string-at-point)
+
+;; 按 "C-<return>" 将光标前的 regexp 转换为可以搜索中文的 regexp.
+(define-key minibuffer-local-map (kbd "C-<return>") 'pyim-cregexp-convert-at-point)
+
+;; 我使用全拼
+(pyim-default-scheme 'quanpin)
+;; (pyim-default-scheme 'wubi)
+;; (pyim-default-scheme 'cangjie)
+
+;; pyim 探针设置
+;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+;; 我自己使用的中英文动态切换规则是：
+;; 1. 光标只有在注释里面时，才可以输入中文。
+;; 2. 光标前是汉字字符时，才能输入中文。
+;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+;; (setq-default pyim-english-input-switch-functions
+;;               '(pyim-probe-dynamic-english
+;;                 pyim-probe-isearch-mode
+;;                 pyim-probe-program-mode
+;;                 pyim-probe-org-structure-template))
+
+;; (setq-default pyim-punctuation-half-width-functions
+;;               '(pyim-probe-punctuation-line-beginning
+;;                 pyim-probe-punctuation-after-punctuation))
+
+;; 开启代码搜索中文功能（比如拼音，五笔码等）
+(pyim-isearch-mode 1)
+
+;; 设置选词框的绘制方式
+(if (posframe-workable-p)
+    (setq pyim-page-tooltip 'posframe)
+  (setq pyim-page-tooltip 'popup))
+
+;; 显示5个候选词。
+(setq pyim-page-length 5)
+
+;; Basedict
+(require 'pyim-basedict)
+(pyim-basedict-enable)
+
+
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp/"))
+(require 'grep-dired)
+(require 'sdcv)
+
+(setq sdcv-dictionary-data-dir "/usr/share/stardict/dic/") ;setup directory of stardict dictionary
+(setq sdcv-dictionary-simple-list    ;setup dictionary list for simple search
+      '("懒虫简明英汉词典"
+        "懒虫简明汉英词典"
+        ))
+(setq sdcv-dictionary-complete-list     ;setup dictionary list for complete search
+      '(
+        "懒虫简明英汉词典"
+        "懒虫简明汉英词典"
+        ))
+
+
+(require 'company-english-helper)
+(global-set-key (kbd "M-k") 'sdcv-search-pointer+)
+
+
+(provide 'init-local)
